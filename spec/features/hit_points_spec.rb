60e7455a84d5e0f7b4139b6a1f40f2fcc52dc15e
Sam <samkitchen94@gmail.com>
2019-05-15 17:45:21 +0100

    refactored using test helpers

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "Testing hit points" do
   scenario "allows users to see opponents hit points" do
-    visit('/')
-    fill_in :player_1_name, with: 'Sam'
-    fill_in :player_2_name, with: 'Yemi'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Yemi: 100HP"
   end
 end
--- spec/features/players_names_spec.rb
+++ spec/features/players_names_spec.rb
@@ -1,9 +1,6 @@
 feature "Testing Players names in forms" do
   scenario "it allows players to enter their names" do
-    visit('/')
-    fill_in :player_1_name, with: 'Sam'
-    fill_in :player_2_name, with: 'Yemi'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Sam vs Yemi'
   end
 end
